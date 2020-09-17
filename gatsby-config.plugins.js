df5db007e781c724fec481c8cb0841b649a31002
Kumar Harsh <kumarharsh241998@gmail.com>
2020-09-17 13:45:38 +0530

    Update gatsby-config.plugins.js

--- gatsby-config.plugins.js
+++ gatsby-config.plugins.js
@@ -16,8 +16,8 @@ module.exports = [
   {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      name: 'Rolwin Reevan',
-      short_name: 'Rolwin Reevan',
+      name: 'Kumar Harsh',
+      short_name: 'Kumar Harsh',
       start_url: '/',
       background_color: '#304CFD',
       theme_color: '#304CFD',
