ddf9620e5b091e51c0f720f1a3aa09d330846a96
Kumar Harsh <kumarharsh241998@gmail.com>
2021-09-18 12:29:08 +0530

    ..

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -10,12 +10,12 @@ const Contact = () => (
     <Layout className="container">
       <SEO
         title="Contact"
-        description="Hello folks Rolwin here. You can contact me through the contact form on this page.
+        description="Hello folks harsh here. You can contact me through the contact form on this page.
                     Please feel free to contact me, don't be shy guys, just remember Rolwin is always open to talk about web
                     technologies especially Javascript techstacks. Currently I'm a part of Gatsby organization on github.
-                    Find me on github - rolwin100."
+                    Find me on github - haarsh24."
         path="/contact"
-        keywords={['Rolwin', 'Reevan', 'Monteiro', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby', 'technology']}
+        keywords={['Kumar', 'Harsh ', 'FullStack developer', 'Javascript', 'ReactJS', 'NodeJS', 'Gatsby', 'technology']}
       />
       <Header />
       <SidebarWrapper>
