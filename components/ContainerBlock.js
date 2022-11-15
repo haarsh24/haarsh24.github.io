b5061e38b518f8b7bd3635dee39243497f4a6818
haarsh24 <kumarharsh241998@gmail.com>
2022-11-15 20:53:51 +0530

    minor changes

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -9,7 +9,7 @@ export default function ContainerBlock({ children, ...customMeta }) {
 
   const meta = {
     title: "Kumar Harsh",
-    description: `I've been developing websites for 5 years straight. Get in touch with me to know more.`,
+    description: `Frontend Developer`,
     image: "/avatar.png",
     type: "website",
     ...customMeta,
@@ -29,12 +29,12 @@ export default function ContainerBlock({ children, ...customMeta }) {
           href={`https://yourwebsite.com${router.asPath}`}
         />
         <meta property="og:type" content={meta.type} />
-        <meta property="og:site_name" content="Manu Arora" />
+        <meta property="og:site_name" content="Kumar Harsh" />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@mannupaaji" />
+        <meta name="twitter:site" content="@codeandchords" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
